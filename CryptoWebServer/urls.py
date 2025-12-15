630c6ab5f0948c548ce79712eebb14f509f11af1
patnamvamsi <patnamvamsi@gmail.com>
2025-12-16 00:13:22 +1100

    Add TA lib support

--- CryptoWebServer/urls.py
+++ CryptoWebServer/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^home/', include('home.urls')),
+    re_path(r'^api/', include('frontend.urls')),
     re_path(r'^', include('home.urls'))
 ]
 
